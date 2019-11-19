2fab9c51f633879509503a12ad6bf631a117e0e8
Kimee <wujm002@galanz.com>
2019-11-19 13:48:23 +0800

    Initial commit

---
+++ Example/KLProgressHUD/KLViewController.h
@@ -0,0 +1,13 @@
+//
+//  KLViewController.h
+//  KLProgressHUD
+//
+//  Created by Kalanhall@163.com on 11/19/2019.
+//  Copyright (c) 2019 Kalanhall@163.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface KLViewController : UIViewController
+
+@end
